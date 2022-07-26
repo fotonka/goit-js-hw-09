6e676855c46745a33164bf9cd717f624bb13afb1
Kirill Chaialo <fotonka098@gmail.com>
2022-07-26 19:21:31 +0300

    1st-in-progress

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startRef = document.querySelector('button, [data-start]');
+const stopRef = document.querySelector('button, [data-stop]');
+const body = document.querySelector('body');
+
+startRef.addEventListener('click', startChanging);
+stopRef.addEventListener('click', stopChanging);
+
+function startChanging() {
+  setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChanging() {
+  clearInterval();
+}
