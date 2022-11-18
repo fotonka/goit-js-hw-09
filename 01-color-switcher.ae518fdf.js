9b07dc6de9e1f1122875a0491f666f859bb0ffc6
fotonka <99725412+fotonka@users.noreply.github.com>
2022-11-18 15:27:46 +0000

    Deploying to gh-pages from @ fotonka/goit-js-hw-09@d2246a476d0e09c595c7ac9fc9555edf1660ded5 🚀

--- 01-color-switcher.ae518fdf.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(d),e.setAttribute("disabled",!0),t.removeAttribute("disabled")}));let d=null;e.setAttribute("disabled",!0);
-//# sourceMappingURL=01-color-switcher.ae518fdf.js.map
